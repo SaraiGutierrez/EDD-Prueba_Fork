f49167a4b4887512aa0b28f75f04a1419ad21fc3
Herberth Avila <ibiem3@gmail.com>
2020-12-13 15:18:33 -0600

    Iniciando metodos del arbol B+

---
+++ b+2.py
@@ -0,0 +1,52 @@
+import math
+import os
+
+# Node creation
+class Node:
+    def __init__(self, order):
+        self.order = order
+        self.values = []
+        self.keys = []
+        self.nextKey = None
+        self.parent = None
+        self.check_leaf = False
+
+    # Insert at the leaf
+    def insert_at_leaf(self, leaf, value, key):
+        if (self.values):
+            temp1 = self.values
+            for i in range(len(temp1)):
+                if (value == temp1[i]):
+                    self.keys[i].append(key)
+                    break
+                elif (value < temp1[i]):
+                    self.values = self.values[:i] + [value] + self.values[i:]
+                    self.keys = self.keys[:i] + [[key]] + self.keys[i:]
+                    break
+                elif (i + 1 == len(temp1)):
+                    self.values.append(value)
+                    self.keys.append([key])
+                    break
+        else:
+            self.values = [value]
+            self.keys = [[key]]
+
+
+# B plus tree
+class BplusTree:
+    def __init__(self, order):
+        self.root = Node(order)
+        self.root.check_leaf = True
+
+
+
+
+bplustree = BplusTree(3)
+bplustree.insert('1', '1')
+bplustree.insert('2', '2')
+bplustree.insert('3', '3')
+bplustree.insert('4', '4')
+bplustree.insert('5', '5')
+bplustree.insert('6', '6')
+bplustree.insert('7', '7')
+bplustree.graficar()
