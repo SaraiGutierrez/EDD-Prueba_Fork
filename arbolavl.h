0b597ed9376a9b7f1c12c0f613dfa75b42ecfb2d
Herberth Avila <ibiem3@gmail.com>
2020-12-12 21:07:07 -0600

    Nuevo Arbol avl

---
+++ arbolavl.h
@@ -0,0 +1,36 @@
+#ifndef ARBOLAVL_H
+#define ARBOLAVL_H
+#include "nodoavl.h"
+
+class ArbolAVL
+{
+public:
+    ArbolAVL();
+    ~ArbolAVL();
+    void insertar(Proyecto* proy);
+    void eliminar(Proyecto* proy);
+    void inOrden(NodoAVL* raiz);
+    NodoAVL* buscar(int id, string nombre);
+    NodoAVL* getRoot();
+    void reporteAVL();
+    void reporteNivelDescendente();
+    void reporteNivelAscendente();
+protected:
+    NodoAVL* root;
+    int max(int a, int b);
+    int altura(NodoAVL* n);
+    NodoAVL* nuevoNodo(Proyecto* proy);
+    NodoAVL* rotarDerecha(NodoAVL* y);
+    NodoAVL* rotarIzquierda(NodoAVL* x);
+    int getBalance(NodoAVL* n);
+    NodoAVL* insertarProyecto(NodoAVL* nodo, Proyecto* proy);
+    NodoAVL* eliminarProyecto(NodoAVL* raiz, Proyecto* proy);
+    NodoAVL* buscarProyecto(NodoAVL* raiz, int id, string n);
+    NodoAVL* nodoMenorValor(NodoAVL* nodo);
+    string getDot();
+    string dot(NodoAVL* raiz);
+    NodoAVL* factorEquilibrio(NodoAVL* raiz);
+    void ordenarNivelesParaReporte(ListaDobleCircular* list, NodoAVL* raiz);
+};
+
+#endif // ARBOLAVL_H
